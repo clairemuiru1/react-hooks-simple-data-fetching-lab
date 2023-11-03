ab1650d062b1134b8560beadbf6e13b493519760
clairemuiru1 <muiruclaire@gmail.com>
2023-11-03 17:27:39 +0300

    "commit changes"

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [dogImage, setDogImage] = useState(null)
+    useEffect(() => {
+        async function fetchDogImage() {
+            const response = await fetch('https://dog.ceo/api/breeds/image/random')
+            const data = await response.json()
+            setDogImage(data.message)
+        }
+        fetchDogImage()
+    },[])
+    return (
+        <>
+            {dogImage ? (
+                <img src={dogImage} alt="A Random Dog" />
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
+    )
+}
+
+export default App
